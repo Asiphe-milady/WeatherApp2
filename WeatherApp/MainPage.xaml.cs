7aa55762917fbb934f66dc5f78cad127a56b892d
ASIPHE NCOMBO <4270171@myuwc.ac.za>
2022-06-28 15:30:52 +0200

    Add project files.

---
+++ WeatherApp/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace WeatherApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await WeathersData();
+        }
+        private async Task WeathersData()
+        {
+            var data = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
+            if (data != PermissionStatus.Granted)
+            {
+                var newdata = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+            }
+            var location = await Geolocation.GetLocationAsync();
+            var latitude = location.Latitude;
+            var longitude = location.Longitude;
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+            string url = $"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&units=metric&appid=1129fdba1b33c9e647f9ab3e80db8052";
+            var response = await client.GetStringAsync(url);
+            var weathersData = JsonConvert.DeserializeObject<DataWeather>(response);
+            BindingContext = weathersData;
+        }
+    }
+}
